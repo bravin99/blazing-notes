d02bde170afe6bbfe79696ef49d27a63935cf52c
bravin99 <bravinshumwe.coding@gmail.com>
2022-10-25 17:21:20 +0300

    add new note endpoint

--- Server/Controllers/NotesController.cs
+++ Server/Controllers/NotesController.cs
@@ -1,7 +1,5 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using blazing_notes.Shared.Models;
+using blazing_notes.Server.Context;
 using Microsoft.AspNetCore.Mvc;
 
 namespace blazing_notes.Server.Controllers
@@ -10,6 +8,29 @@ namespace blazing_notes.Server.Controllers
     [Route("api/[controller]")]
     public class NotesController : ControllerBase
     {
-        
+        private readonly BlazingNotesDatabaseContext _context;
+
+        public NotesController(BlazingNotesDatabaseContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost("new")]
+        public async Task<ActionResult<Note>> NewNote([FromBody] NoteDto request)
+        {
+            Note note = new Note(){
+                Title = request.Title,
+                Content = request.Content,
+                Tags = request.Tags,
+                Created = DateTime.UtcNow
+            };
+            if (ModelState.IsValid)
+            {
+                await _context.Notes!.AddAsync(note);
+                await _context.SaveChangesAsync();
+                return Ok(note);
+            }
+            return BadRequest();
+        }
     }
 }
