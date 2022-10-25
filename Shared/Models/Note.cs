5df7e9336fb88f053cf4a3e9db73b31d09308acf
bravin99 <bravinshumwe.coding@gmail.com>
2022-10-25 16:49:23 +0300

    models

---
+++ Shared/Models/Note.cs
@@ -0,0 +1,28 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Shared.Models
+{
+    public class Note
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [StringLength(50, ErrorMessage="Title is too long")]
+        public string? Title { get; set; }
+        [Required]
+        public string?  Content { get; set; }
+        public virtual List<Tag>? Tags { get; set; }
+        public DateTime Created { get; set; }
+        public DateTime Updated { get; set; }
+    }
+
+    public class Tag
+    {
+        public int Id { get; set; }
+        [Required]
+        public string? Name { get; set; }
+        public string?  Description { get; set; }
+        public DateTime Added { get; set; }
+    }
+}
